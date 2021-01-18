be875c8daab54ded172eb4295f8d0027eefa368b
Tebogo <tebzax2@gmail.com>
2021-01-18 15:23:06 +0200

    feat:able to move newly downloaded files to correct folder

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 import shutil
+import time
+from pathlib import Path  
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
 
 downloads_folder = "/users/Tebza/Desktop/test_folder"
 directory = os.listdir(downloads_folder)
 
-# File extensions according to their folder  type
+# File extensions according to their folder type
 folder_extensions = {
     'Documents' : ("doc","docx","html","htm","odt","pdf","xls","xlsx","ods","ppt" "pptx","txt"),
     'Images' : ("jpg","png","gif","webp","tiff","psd","raw","bmp","heif","indd","jpeg"),
@@ -44,27 +48,57 @@ def create_folder(name):
     except OSError:
         print("{} folder already exists.".format(name))
 
-def create_needed_folders (extensions):
-    print("creating folders")
-    for ext in extensions:
-        #get the object key that its value has the ext.
-        for name , exts in folder_extensions.items():
-            if ext in exts:
+def sort_file(filename):
+    file_path = f"{downloads_folder}/{filename}"
+    ext = os.path.splitext(filename)[1][1:].lower()
+    for name , exts in folder_extensions.items():
+        if ext in exts:
+            if os.path.exists(f"{downloads_folder}/{name}"):
+                try:
+                    shutil.move(file_path,f"{downloads_folder}/{name}")
+                except:
+                    print(f"error occured while moving {filename} to folder {name}")
+            else:
                 create_folder(name)
+                try:
+                    shutil.move(file_path,f"{downloads_folder}/{name}")
+                except:
+                    print(f"error occured while moving {filename} to folder {name}")
 
-def move_files():
+
+def sort_files():
     print("moving files")
     for filename in files_only:
-        file = "{}/{}".format(downloads_folder,filename)
-        ext = os.path.splitext(filename)[1][1:].lower()
-        for name , exts in folder_extensions.items():
-            if ext in exts:
-                try:
-                    shutil.move(file,"{}/{}".format(downloads_folder,name))
-                except:
-                    print("error occured while moving file {} to folder {}".format(filename, name))
+        sort_file(filename)
 
-create_needed_folders(get_all_file_extensions())
-move_files()
+sort_files()
 
 print("Done")
+
+
+def on_created(event):
+    sort_file(Path(event.src_path).name)
+
+if __name__ == "__main__":
+
+    patterns = "*"
+    ignore_patterns = [".DS_Store"]
+    ignore_directories = True
+    case_sensitive = True
+    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+    my_event_handler.on_created = on_created
+    my_event_handler.on_modified = on_created
+    go_recursively = False
+    my_observer = Observer()
+    my_observer.schedule(my_event_handler,downloads_folder, recursive=go_recursively)
+    my_observer.start()
+
+    print("Watching new files...")
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        my_observer.stop()
+        print("Stopped watching files.")
+    my_observer.join() 
+
