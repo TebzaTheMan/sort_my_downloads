21850fe08c525138e76e672051500c2887ceb503
Tebogo <tebzax2@gmail.com>
2021-01-22 13:55:01 +0200

    feat : add function to get download path for any OS.

--- main.py
+++ main.py
@@ -21,7 +21,17 @@ folder_extensions = {
     'Executables' : ("apk","bat","bin","cgi","pl","exe","jar","msi","py","wsf"),
     'fonts' : ("fnt","fon","otf","ttf"),
 }
-
+def get_download_path():
+    if os.name == 'nt':
+        import winreg
+        sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+        downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+            location = winreg.QueryValueEx(key, downloads_guid)[0]
+        return location
+    else:
+        return str(Path.home() / "Downloads")
+        
 def get_only_files_in_dir ():
     print("getting files")
     files_only = []
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+from pathlib import Path  
+import os
+
+def get_download_path():
+    if os.name == 'nt':
+        import winreg
+        sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+        downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+            location = winreg.QueryValueEx(key, downloads_guid)[0]
+        return location
+    else:
+        return str(Path.home() / "Downloads")
+
+print(get_download_path())
